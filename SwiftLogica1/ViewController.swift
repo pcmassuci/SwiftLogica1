3a4eb11eda0ddde10c4879c8f46f2f031c975a39
Paulo César Morandi Massuci <pcmassuci@gmail.com>
2018-07-19 14:46:45 -0300

    Commit inicial

---
+++ SwiftLogica1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLogica1
+//
+//  Created by Paulo César Morandi Massuci on 19/07/2018.
+//  Copyright © 2018 Paulo César Morandi Massuci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
